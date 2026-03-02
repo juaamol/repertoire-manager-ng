577326e3e0132abb4b1632860fc2834201ce74a6
jambulud <jambulud@gmail.com>
2026-03-03 00:50:50 +0100

    Login button

--- src/app/pages/login/login.ts
+++ src/app/pages/login/login.ts
@@ -1,11 +1,9 @@
 import { Component } from '@angular/core';
-import { PageTitle } from '../../page-title/page-title';
-import { ClefInput } from '../../components/clef-input/clef-input';
 import { ClefLoginForm } from './clef-login-form/clef-login-form';
 
 @Component({
   selector: 'app-login',
-  imports: [PageTitle, ClefLoginForm],
+  imports: [ClefLoginForm],
   templateUrl: './login.html',
   styleUrl: './login.scss',
 })
